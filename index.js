85fbe0288b7216bd902a1902d702f4b8ba582b3b
AlcantarAdrn <adriian.alcantar@gmail.com>
2021-03-02 10:58:11 +0000

    StarWarAPI

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ const axios = require('axios');
 
 http
   .createServer(function(req, res) {
-    res.write(users.join("\n")); //display the list of users on the page
-   res.write("\n\n"+emails.join("\n")); //display the list of users on the page
-   
+    res.write(characters.join("\n")); //display the list of users on the page
+
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
 
 
-let users = [];
-let emails = [];
+let characters = [];
 
 (async function getNames(){
   try{
-    const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
-    users = data.map(user=>user.name);
-    emails = data.map(email=>email.email);
-    console.log(users);
-    console.log(emails);
-  } catch(error){
+    const {data} = await axios.get(
+        "https://swapi.dev/api/people/"
+    );
+    characters = data.results.map(character => character.name);
+    console.log(characters);
+   } catch(error){
     console.log(error)
   }
-})()
+})();
