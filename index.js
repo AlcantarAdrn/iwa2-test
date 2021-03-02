d2649515b7ffa36896781e37c3c4c26faa6ab9ae
AlcantarAdrn <adriian.alcantar@gmail.com>
2021-03-02 10:19:06 +0000

    week3Code

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const http = require('http');
+const axios = require('axios');
 
-http.createServer((req, res)=>{
-  res.write("Hello WebDeve \n"); // write a response
-  res.end(); //end the response
-}).listen(8000); // listen for requests on port 8000
+http
+  .createServer(function(req, res) {
+    res.write(users.join("\n")); //display the list of users on the page
+   res.write("\n\n"+emails.join("\n")); //display the list of users on the page
+   
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
+
+
+let users = [];
+let emails = [];
+
+(async function getNames(){
+  try{
+    const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
+    users = data.map(user=>user.name);
+    emails = data.map(email=>email.email);
+    console.log(users);
+    console.log(emails);
+  } catch(error){
+    console.log(error)
+  }
+})()
