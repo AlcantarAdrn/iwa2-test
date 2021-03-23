fe7eb1ef0f73dab295d9519b6574f1e9e83d46b2
AlcantarAdrn <adriian.alcantar@gmail.com>
2021-03-23 11:22:57 +0000

    all done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ axios = require('axios'),
 logger = require('morgan'),
 cors = require('cors'),
 express = require('express'),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser'),
+mongoose = require('mongoose');
 
 var app =express();
 var port = 8000;
@@ -21,23 +22,37 @@ app.use(require('./routes'));
 //   .listen(8080); //the server object listens on port 8080
 
 
-let characters = [];
+// let characters = [];
 
-(async function getNames(){
-  try{
-    const {data} = await axios.get(
-        "https://swapi.dev/api/people/"
-    );
-    characters = data.results.map(character => character.name);
-    console.log(characters);
-   } catch(error){
-    console.log(error)
-  }
-})();
+// (async function getNames(){
+//   try{
+//     const {data} = await axios.get(
+//         "https://swapi.dev/api/people/"
+//     );
+//     characters = data.results.map(character => character.name);
+//     console.log(characters);
+//    } catch(error){
+//     console.log(error)
+//   }
+// })();
 
-app.listen(port, function(err){
+// mongoose.connect('mongodb://localhost/test');
 
-    console.log('Listening on port:' + port);
+// mongoose.connection.on('error', (err) => { 
+//     console.log('Mongodb Error: ', err); 
+//     process.exit();
+// });
+// mongoose.connection.on('connected', () => { 
+//     console.log('MongoDB is successfully connected');
+// });
 
+// app.listen(port, function(err){
 
-});
+//     console.log('Listening on port:' + port);
+
+const dbURI = "mongodb://localhost/test";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((result) => console.log('connected to db'))
+        .catch((err) => console.log(err));
+
+ 
--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express'),
 router = express.Router();
 
 var itemCtrl = require('./item-controller');
-
+var userCtrl = require('./user-controller')
 router.get('/hello', itemCtrl.getWorld);
 router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
 router.post('/hello', itemCtrl.postWorld);
